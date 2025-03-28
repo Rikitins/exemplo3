616ada91f7886dcae2f066a9bd0a10c32309e3a6
Henrique <hlm_2005@outlook.com>
2025-03-28 11:41:49 -0300

    declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+// alert("Hello world")
+// Declaração e Variaveis
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =19
+console.log(idade)
+
+const sobrenome = "fiapinho"
+console.log(sobrenome)
+
+//undefined
+
+let exemplo;
+console.log(exemplo)
+
+let exemplo1= null;
+console.log(exemplo1)
+
+//tipos de variaveis
+
+let
